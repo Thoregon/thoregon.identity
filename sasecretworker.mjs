c7f9d253396fe3b502437252e376571f3ce616ea
Bernhard Lukassen <bl@bernhard-lukassen.com>
2022-03-21 16:26:07 +0100

    refactoring: thoregon.crystaline rename provider -> consumer, service -> producer

--- sasecretworker.mjs
+++ sasecretworker.mjs
@@ -7,7 +7,7 @@
  */
 
 import Facade         from "/thoregon.crystalline/lib/facade.mjs";
-import JSProvider     from "/thoregon.crystalline/lib/providers/jsprovider.mjs";
+import JSConsumer     from "/thoregon.crystalline/lib/consumers/jsconsumer.mjs";
 import IdentitySecret from "./lib/identitysecret.mjs";
 
-export default Facade.use(await JSProvider.with(new IdentitySecret()));
+export default Facade.use(await JSConsumer.with(new IdentitySecret()));
