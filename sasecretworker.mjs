702a7380ac37818149f7287103e71118fe7362d2
Bernhard Lukassen <bl@bernhard-lukassen.com>
2024-02-19 10:42:40 +0100

    browser version fixes

--- sasecretworker.mjs
+++ sasecretworker.mjs
@@ -10,4 +10,4 @@ import Facade         from "/thoregon.crystalline/lib/facade.mjs";
 import JSConsumer     from "/thoregon.crystalline/lib/consumers/jsconsumer.mjs";
 import IdentitySecret from "./lib/identitysecret.mjs";
 
-export default Facade.use(await JSConsumer.with(new IdentitySecret()));
+export default async () => Facade.use(await JSConsumer.with(new IdentitySecret()));
